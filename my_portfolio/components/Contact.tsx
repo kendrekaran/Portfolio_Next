89c0a183c6203329afd802f9b56234ea9b0c44cf
Karan kendre <142140048+kendrekaran@users.noreply.github.com>
2024-12-07 20:04:37 +0530

    Add files via upload

---
+++ my_portfolio/components/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignupFormDemo from './signup-form-demo'
+
+const Contact = () => {
+  return (
+    <div className='py-10 sm:py-0'>
+        <SignupFormDemo/>
+    </div>
+  )
+}
+
+export default Contact
